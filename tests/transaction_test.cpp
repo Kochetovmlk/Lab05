94f837edf30fb4398bb335396d64e74a5d5fdbfc
Kochetov.mlk <mikhail.kochetov.04@list.ru>
2023-05-17 12:04:53 +0300

    Configure build and coverage

---
+++ tests/transaction_test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "transaction.h"
+
+// Ваш код модульных тестов для класса Transaction
+// Используйте mock-объекты для необходимых зависимостей
